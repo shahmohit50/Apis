34a8fadef3c8239f945c48de5cab45d855b06fc7
Mohitraj Shah <shahmohit50@gmail.com>
2023-05-25 15:49:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,216 @@
+var admin = require("firebase-admin");
+const cors = require('cors');
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mch-app-3b8de-default-rtdb.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors()); 
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  const userRef = db.collection('Users');
+
+  userRef.add(user)
+    .then((docRef) => {
+      res.status(201).json({
+        message: 'User added successfully',
+        id: docRef.id
+      });
+    })
+    .catch((error) => {
+      console.error('Error adding new user:', error);
+      res.status(500).json({
+        error: 'Error adding new user'
+      });
+    });
+});
+
+  //Get Service
+  // app.get('/documents', async (req, res) => {
+  //    try {
+  //    const documentRef = db.collection('Childs');
+  //    const query = documentRefn.where('FirstName', '==', 'Anvi')
+  //    const snapshot = await query.get().then(snapshot => {
+
+  //    });
+  //   //  const documents = [];
+  //   //  snapshot.forEach(doc => { 
+  //   //  documents.push({ id: doc.id, ...doc.data() });
+  //    });
+  //    res.status(200).json(documents);
+
+  //   } catch (error) {
+  //    console.error(error);
+  //    res.status(500).send('Server error');
+  //    }
+  //   });
+  
+  //Completed Vaccine
+   app.get('/CompletedVaccine', async (req, res) => 
+  { // Get collection data with where clause 
+    const collection = admin.firestore().collection('VaccineDetails'); 
+    const currentDate = new Date();
+    const query = collection.where('ScheduledOn', '!=', ' ').where('Date', '!=', ' '); 
+    const result = await query.get().then(snapshot => { 
+      return snapshot.docs.map(doc => doc.data());
+     }); 
+     
+     res.json(result); });
+
+
+
+     //Vaccine Taken
+     app.get('/VaccineTaken', async (req, res) => {
+      try {
+          const collection1Ref = db.collection('VaccineDetails');
+          const collection2Ref = db.collection('Childs');
+  
+          const snapshot1 = await collection1Ref.get();
+  
+          const result = [];
+          for (const doc1 of snapshot1.docs) {
+              const data1 = doc1.data();
+              if (data1.ScheduledOn) 
+              {
+                if(data1.Date)
+                {
+              const snapshot2 = await collection2Ref.where('ChildId', '==', data1.patientID).get();
+  
+              for (const doc2 of snapshot2.docs) {
+                  const data2 = doc2.data();
+  
+                  const combinedData = {
+                      ...data1,
+                      ...data2
+                  };
+                  result.push(combinedData);
+              }
+            }
+            }
+          }
+          res.status(200).json(result);
+      } catch (error) {
+          console.error(error);
+          res.status(500).send('Server error');
+      }
+  });
+
+
+   //upcoming Vaccine
+   app.get('/UpcomingVaccine', async (req, res) => {
+    try {
+        const collection1Ref = db.collection('VaccineDetails');
+        const collection2Ref = db.collection('Childs');
+        
+        const snapshot1 = await collection1Ref.get();
+        const currentDate = new Date(); 
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+         const formattedDate = currentDate.toLocaleDateString('en-US', options);
+         
+        const result = [];
+        for (const doc1 of snapshot1.docs) {
+            const data1 = doc1.data();
+            if (data1.ScheduledOn) 
+            {
+            const ScheduleOn = new Date(data1.ScheduledOn);
+          
+            const Scheduledate = { year: 'numeric', month: '2-digit', day: '2-digit' };
+            const formatedSchDate = ScheduleOn.toLocaleDateString('en-US', Scheduledate);
+            console.log(formatedSchDate)
+            
+       
+         
+            if (formatedSchDate  >= formattedDate ) 
+            {
+               if(!data1.Date)
+              {
+            const snapshot2 = await collection2Ref.where('ChildId', '==', data1.patientID).get();
+
+            for (const doc2 of snapshot2.docs) {
+                const data2 = doc2.data();
+
+                const combinedData = {
+                    ...data1,
+                    ...data2
+                };
+                result.push(combinedData);
+            }
+          }
+          }
+        }
+        }
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+
+ //Due Vaccine
+ app.get('/DueVaccine', async (req, res) => {
+  try {
+      const collection1Ref = db.collection('VaccineDetails');
+      const collection2Ref = db.collection('Childs');
+      
+      const snapshot1 = await collection1Ref.get();
+      const currentDate = new Date(); 
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+       const formattedDate = currentDate.toLocaleDateString('en-US', options);
+       
+      const result = [];
+      for (const doc1 of snapshot1.docs) {
+          const data1 = doc1.data();
+          if (data1.ScheduledOn) 
+          {
+          const ScheduleOn = new Date(data1.ScheduledOn);
+        
+          const Scheduledate = { year: 'numeric', month: '2-digit', day: '2-digit' };
+          const formatedSchDate = ScheduleOn.toLocaleDateString('en-US', Scheduledate);
+          console.log(formatedSchDate)
+          
+     
+       
+          if (formatedSchDate  < formattedDate ) 
+          {
+             if(!data1.Date)
+            {
+          const snapshot2 = await collection2Ref.where('ChildId', '==', data1.patientID).get();
+
+          for (const doc2 of snapshot2.docs) {
+              const data2 = doc2.data();
+
+              const combinedData = {
+                  ...data1,
+                  ...data2
+              };
+              result.push(combinedData);
+          }
+        }
+        }
+      }
+      }
+      res.status(200).json(result);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
